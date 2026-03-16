74112524c9a422098f5bf022ecf923a399a574a7
Federico <65908512+coval3nte@users.noreply.github.com>
2026-03-16 23:35:08 +0000

    feat(examples): add bun

---
+++ examples/bun/index.ts
@@ -0,0 +1,12 @@
+import * as prem_rs from "@premAI-io/prem-rs";
+
+const {
+    ATTESTATION_SERVER
+} = process.env;
+
+if (!ATTESTATION_SERVER) throw new Error("missing ATTESTATION_SERVER...");
+
+
+const client = new prem_rs.ClientBuilder(ATTESTATION_SERVER).build();
+// const modules = await client.request_modules();
+await client.attest();
